0b6e805903f50b02f532d9108cc5ffcd6628f18e
subhendukhatuya <subha.cse143@gmail.com>
2023-04-04 13:03:07 +0530

    add script to convert data into BIO

---
+++ create_flair_format_ner_data.py
@@ -0,0 +1,26 @@
+import json
+
+import pandas as pd
+
+df = pd.read_csv('./train_finer.csv')
+df_result = pd.DataFrame(columns=['text', 'annotation'])
+
+with open('./labels.json', 'r') as JSON:
+    label_dict = json.load(JSON)
+
+xbrl_label_dict = {v: k for k, v in label_dict.items()}
+
+f_train = open('./train.txt', 'w')
+tokens_length = []
+for i, row in df.iterrows():
+    tokens = eval(row['tokens'])
+    tags = eval(row['ner_tags'])
+
+    if len(tokens) <= 50:
+
+        for token_index in range(len(tokens)):
+            f_train.write(tokens[token_index] + ' ' + xbrl_label_dict[tags[token_index]])
+            f_train.write('\n')
+        f_train.write('\n')
+
+# print(set(sorted(tokens_length)))
